a940026d039b1d5ee660f21d0c7cee6be129e41c
Sam Croft <samcroft@gmail.com>
2011-12-03 10:00:13 +0000

    Added version for PhoneGap 1.00+

---
+++ iOS app/PhoneGap-1.00+/Loading data into PhoneGap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Loading data into PhoneGap
+//
+//  Created by Samuel Croft on 03/12/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
